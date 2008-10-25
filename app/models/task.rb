6af8d6d983d24c150a46d9fed2a3a6542459ed59
simo <simo@Mentat.local>
2008-10-25 18:08:44 +0200

    a

--- app/models/task.rb
+++ app/models/task.rb
@@ -52,8 +52,10 @@ class Task < ActiveRecord::Base
     @doc = self.document
     current_pages = @doc.pages.size
     files.each_with_index do |f,i|
-      p = Page.create :number => (current_pages+i+1), :document_id => @doc.id
-      Image.create :uploaded_data => ActionController::TestUploadedFile.new("#{f}", MIME::Types.type_for(f)), :page_id => p.id
+      if [".jpg",".jpeg",".gif",".png"].include? File.extname(f)
+        p = Page.create :number => (current_pages+i+1), :document_id => @doc.id
+        Image.create :uploaded_data => ActionController::TestUploadedFile.new("#{f}", MIME::Types.type_for(f)), :page_id => p.id
+      end
     end
     
     # Set the document's size automatically
