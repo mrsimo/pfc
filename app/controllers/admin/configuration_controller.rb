7a9cc9a27471a75c31047e3584a1b2324def69cf
simo <simo@Mentat.local>
2008-11-22 17:39:22 +0100

    monitor del proceso de conversion

--- app/controllers/admin/configuration_controller.rb
+++ app/controllers/admin/configuration_controller.rb
@@ -2,7 +2,9 @@ class Admin::ConfigurationController < ApplicationController
   before_filter :login_required
   
   def index
+    require "daemons"
     @configurations = Configuration.find :all
+    @monitor = Daemons::Monitor.find "log","processor"
   end
 
   def update
@@ -17,7 +19,36 @@ class Admin::ConfigurationController < ApplicationController
     redirect_to :action => :index
   end
   
+  def start_daemon
+    require "daemons"
+    @monitor = Daemons::Monitor.find "log","processor"
+    if @monitor.nil?
+      `lib/daemons/processor_ctl start`
+    end
+    flash[:notice] = "El proceso se ha iniciado."
+    begin
+      redirect_to :back
+    rescue Exception => e
+      redirect_to admin_configurations_path 
+    end
+  end
+  
+  def stop_daemon
+    require "daemons"
+    @monitor = Daemons::Monitor.find "log","processor"
+    unless @monitor.nil?
+      `lib/daemons/processor_ctl stop`
+    end
+    flash[:notice] = "El proceso ha recibido la orden de parar. Parará cuando acabe con el actual documento."
+    begin
+      redirect_to :back
+    rescue Exception => e
+      redirect_to admin_configurations_path 
+    end
+  end
+  
   def authorized?
     current_user.id == 1
-  end
+  end  
+  
 end
